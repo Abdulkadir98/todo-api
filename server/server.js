dc168e92ff4243ffb3ff87e6e2ae94112888e0c8
Abdulkadir98 <ak98.narutokun@gmail.com>
2017-08-15 12:42:50 +0530

    Added token generation

--- server/server.js
+++ server/server.js
@@ -88,7 +88,22 @@ app.patch('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     });
+});
 
+app.post('/users', (req,res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    var user = new User(body);
+    user.save().then(() => {
+        // res.send(user);
+        return user.generateAuthToken();
+
+
+    }, (e) => {
+        res.status(400).send(e);
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    });
 });
 
 app.listen(port, () => {
